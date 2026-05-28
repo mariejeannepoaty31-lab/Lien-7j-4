44cb6cdafe4e841ba88dc87c72cab53cede472ab
mariejeannepoaty31-lab <mariejeannepoaty31@gmail.com>
2026-05-28 08:23:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Ça marche ! Ton app est en ligne sur Render 🎉');
+app.get('/', async (req, res) => {
+  try {
+    const response = await axios.get('https://iptv-org.github.io/iptv/index.country.m3u');
+    res.setHeader('Content-Type', 'application/x-mpegurl');
+    res.send(response.data);
+  } catch (err) {
+    res.status(500).send('Erreur lors du chargement de la playlist');
+  }
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+app.listen(PORT, () => console.log(`App running on ${PORT}`));
